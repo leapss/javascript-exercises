e3aabcba4079c85bdac93c4d973892bda7d7db6a
leapss <leandropetrini29@gmail.com>
2024-04-25 16:42:07 -0300

    half way

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (n1 < 0 || n2 < 0 || typeof n1 != 'number' || typeof n2 != 'number'){
+        return 'ERROR'
+    } else {
+        const start = Math.min(n1, n2);
+        const end = Math.max(n1, n2);
+        let sum = 0;
+        
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    };
 };
 
 // Do not edit below this line

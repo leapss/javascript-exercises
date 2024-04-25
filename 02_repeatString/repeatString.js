a69f7130b60eab015f06c6ea88551788d07193c7
leapss <leandropetrini29@gmail.com>
2024-04-25 19:29:24 -0300

    All test done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,5 @@
 const repeatString = function(string, number) {
+    if (number < 0) return "ERROR";
     let result = ''
     for (let i = 0; i < number; i++) {
         result += string;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const today = new Date().getFullYear();
+    return people.reduce((oldest, person) => {
+        const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : today - person.yearOfBirth;
+        const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : today - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+        });
+            
 };
 
 // Do not edit below this line
